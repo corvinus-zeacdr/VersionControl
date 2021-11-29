d731f7397c65d25cc6b7c91c3f87e7a7de324571
corvinus-zeacdr <mate.kodobocz@stud.uni-corvinus.hu>
2021-11-29 10:54:37 +0100

    steps

---
+++ Gyak10_ZEACDR/Gyak10_ZEACDR/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyak10_ZEACDR.Entities
+{
+    public class DeathProbability
+    {
+        public Gender DeathGender { get; set; }
+        public int DeathAge { get; set; }
+        public double DProbability { get; set; }
+    }
+}
