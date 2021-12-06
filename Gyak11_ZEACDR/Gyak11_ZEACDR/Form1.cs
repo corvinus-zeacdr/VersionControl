9f49405a39bd826eb6bab1b6301e9522e1a26f69
corvinus-zeacdr <mate.kodobocz@stud.uni-corvinus.hu>
2021-12-06 15:24:21 +0100

    steps

--- Gyak11_ZEACDR/Gyak11_ZEACDR/Form1.cs
+++ Gyak11_ZEACDR/Gyak11_ZEACDR/Form1.cs
@@ -21,7 +21,7 @@ namespace Gyak11_ZEACDR
         int nbrOfStepsIncrement = 10;
         int generation = 1;
 
-
+        Brain winnerBrain = null;
 
         public Form1()
         {
@@ -41,10 +41,7 @@ namespace Gyak11_ZEACDR
             }
             gc.Start();
 
-            var playerList = from p in gc.GetCurrentPlayers()
-                             orderby p.GetFitness() descending
-                             select p;
-            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
         }
 
         private void Gc_GameOver(object sender)
@@ -53,6 +50,48 @@ namespace Gyak11_ZEACDR
             label1.Text = string.Format(
                 "{0}. generáció",
                 generation);
+
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+            ga.Focus();
+            gc.Start(true);
         }
     }
 }
